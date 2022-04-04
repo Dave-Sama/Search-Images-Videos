8ab1bd7b3e5aee787647903c477f3ea4c912b363
Dave-Sama <davech1228@gmail.com>
2022-04-04 19:58:59 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
+import styledComponents from 'styled-components';
+import tw from 'twin.macro';
+
 import './App.css';
+const AppContainer = styledComponents.div`
+  ${tw`lg:flex-row`}
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	align-items: stretch;
+	align-content: stretch;
+`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const TopContainer = styledComponents.div`
+	flex-grow:1;
+	flex-shrink: 1;
+	flex-basis: auto;
+	align-self: auto;
+	order: 0;
+	${tw`
+    bg-red-200
+    
+  `}
+`;
+
+const BottomContainer = styledComponents.div`
+	${tw`bg-blue-200 
+  flex
+  h-screen
+  `}
+  
+	flex-grow: 4;
+	flex-shrink: 1;
+	flex-basis: auto;
+	align-self: auto;
+	order: 0;
+`;
+
+const TitleContainer = styledComponents.h1`
+  ${tw`
+    font-size[30px]
+    m-auto
+  `}
+`;
+export default function App() {
+	return (
+		<AppContainer>
+			<TopContainer>hell0</TopContainer>
+			<BottomContainer>
+				<TitleContainer>World</TitleContainer>
+			</BottomContainer>
+		</AppContainer>
+	);
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
