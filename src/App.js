233a4aeebd7e967fe61e1db11119d8415b519b03
Dave-Sama <davech1228@gmail.com>
2022-04-05 12:13:02 +0300

    finished with the inputs

--- src/App.js
+++ src/App.js
@@ -16,13 +16,14 @@ const AppContainer = styledComponents.div`
 
 const TopContainer = styledComponents.div`
 	
-	flex-basis: 100px;
+	flex-basis: 80px;
 	flex-grow: 1;
 	
 	${tw`
 	h-screen
-	
 	flex
+	flex-row
+	lg:flex-col
     bg-red-200
   	`}
 `;
@@ -41,10 +42,34 @@ const BottomContainer = styledComponents.div`
 	order: 0;
 `;
 
-const TitleContainer = styledComponents.h1`
+const Input = styledComponents.input`
+	${tw`
+		placeholder:text-center
+		w-5/12
+		h-10
+		rounded-xl
+		shadow-md
+		lg:w-11/12 
+		bg-green-200
+		focus:bg-red-400
+		m-auto
+		focus:transition-all
+		
+	`}
+`;
+
+const TitleContainer = styledComponents.div`
   ${tw`
-    font-size[30px]
+    font-size[20px]
     m-auto
+	max-h-full
+	lg:h-1/6
+	bg-purple-200
+	flex
+	flex-row
+	lg:flex-col
+	w-full
+	lg:m-0
   `}
 `;
 export default function App() {
@@ -52,10 +77,13 @@ export default function App() {
 		<AppContainer>
 			<TopContainer>
 				{' '}
-				<TitleContainer>Hello</TitleContainer>
+				<TitleContainer>
+					<Input placeholder='Type here1' />
+					<Input placeholder='Type here2' />
+				</TitleContainer>
 			</TopContainer>
 			<BottomContainer>
-				<TitleContainer>World</TitleContainer>
+				{/* <TitleContainer>World</TitleContainer> */}
 			</BottomContainer>
 		</AppContainer>
 	);
